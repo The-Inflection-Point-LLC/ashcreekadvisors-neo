a645db9df9d58af8376091c2bb4cdcd3470b45a5
Muhammad Usman <usman.hashmi93@gmail.com>
2025-08-02 23:54:46 +0200

    build-update

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/usman/Documents/ashcreekadvisors-neo/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
